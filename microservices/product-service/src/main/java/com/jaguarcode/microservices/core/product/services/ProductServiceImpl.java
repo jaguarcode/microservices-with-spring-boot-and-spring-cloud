c2ba5d027cd62a3433553b1b98674a6dcc09a7f5
IKHYEON KIM <behonz@gmail.com>
2021-05-06 20:41:56 +0900

    초기 설정 후 마이크로서비스 셋 생성

---
+++ microservices/product-service/src/main/java/com/jaguarcode/microservices/core/product/services/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package com.jaguarcode.microservices.core.product.services;
+
+import com.jaguarcode.api.core.product.Product;
+import com.jaguarcode.util.exceptions.InvalidInputException;
+import com.jaguarcode.util.exceptions.NotFoundException;
+import com.jaguarcode.util.http.ServiceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.jaguarcode.api.core.product.ProductService;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+        if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
+
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    }
+}
